2b988f9ddae795e6951c9141ca9f7648991395b0
Jae-Nadine <jae.nadine@icloud.com>
2024-06-10 01:01:44 -0700

    Update UserServiceExercise.java

--- UserServiceExercise.java
+++ UserServiceExercise.java
@@ -10,9 +10,21 @@ public class UserServiceExercise {
 		User user = new User(username, password, name);
 		return user;
 	}
-		
-
-	}
 
 
+    public static String[] parseText(String input) {
+        return input.split(",");
+    }
 
+    public static void main(String[] args) {
+        String[] inputArray = {"Scrumptious321", "Password123", "Patunia"};
+        User createdUser = createUser(inputArray);
+        System.out.println("Created user: " + createdUser.getUsername() + ", " + 
+        createdUser.getPassword() + ", " + createdUser.getName());
+
+        String inputString = "Scrumptious321,Password123,Patunia";
+        String[] parsedArray = parseText(inputString);
+        System.out.println("Parsed array: " + parsedArray[0] + ", " + parsedArray[1] + ", " + 
+        parsedArray[2]);
+    }
+}
